0c366711aee6205f5929bc3b99c16b7f1b76c0f7
stephen <stephen.zhu@sap.com>
2017-08-20 15:53:46 +0800

    forms

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from collection.models import Thing
 
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description', )
+    prepopulated_fields = {'slug':('name', )}
 # Register your models here.
+admin.site.register(Thing, ThingAdmin)
--- collection/views.py
+++ collection/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-
+from collection.models import Thing
+from collection.forms import ThingForm
 # Create your views here.
 def index(request):
-    number = 6
-    thing = "Thing name"
+    things = Thing.objects.all()
     return render(request, 'index.html',{
-        'number': number,
+        'things':things,
+    })
+def thing_detail(request, slug):
+    thing = Thing.objects.get(slug = slug)
+
+    return render(request, 'things/thing_detail.html', {
         'thing':thing,
+    })
+def edit_thing(request, slug):
+    thing = Thing.objects.get(slug = slug) 
+    form_class = ThingForm
+    if request.method == 'POST':
+        form = form_class(data = request.POST, instance = thing)
+        if form.is_valid():
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+    else:
+        form = form_class(instance = thing)
+    return render(request, 'things/edit_thing.html',{
+        'thing': thing,
+        'form':form,
     })
